05bf60ffc0fe1ee7d63060483ce4edde9655d66a
Markus Damm <44026453+MarkusDamm@users.noreply.github.com>
2021-10-14 15:49:25 +0200

    implement laser rotation

--- Laser League/Script/Build/Script.js
+++ Laser League/Script/Build/Script.js
@@ -1,1 +1,59 @@
-console.warn("%cFor scripting, start your favorite IDE (e.g. VSCode) with typescript installed, load and compile this project", "color: red; font-weight: bold;");
+"use strict";
+var Script;
+(function (Script) {
+    var ƒ = FudgeCore;
+    ƒ.Project.registerScriptNamespace(Script); // Register the namespace to FUDGE for serialization
+    class CustomComponentScript extends ƒ.ComponentScript {
+        constructor() {
+            super();
+            // Properties may be mutated by users in the editor via the automatically created user interface
+            this.message = "CustomComponentScript added to ";
+            // Activate the functions of this component as response to events
+            this.hndEvent = (_event) => {
+                switch (_event.type) {
+                    case "componentAdd" /* COMPONENT_ADD */:
+                        ƒ.Debug.log(this.message, this.node);
+                        break;
+                    case "componentRemove" /* COMPONENT_REMOVE */:
+                        this.removeEventListener("componentAdd" /* COMPONENT_ADD */, this.hndEvent);
+                        this.removeEventListener("componentRemove" /* COMPONENT_REMOVE */, this.hndEvent);
+                        break;
+                }
+            };
+            // Don't start when running in editor
+            if (ƒ.Project.mode == ƒ.MODE.EDITOR)
+                return;
+            // Listen to this component being added to or removed from a node
+            this.addEventListener("componentAdd" /* COMPONENT_ADD */, this.hndEvent);
+            this.addEventListener("componentRemove" /* COMPONENT_REMOVE */, this.hndEvent);
+        }
+    }
+    // Register the script as component for use in the editor via drag&drop
+    CustomComponentScript.iSubclass = ƒ.Component.registerSubclass(CustomComponentScript);
+    Script.CustomComponentScript = CustomComponentScript;
+})(Script || (Script = {}));
+var Script;
+(function (Script) {
+    // Don't forget to compile: Strg + Shift + B
+    var ƒ = FudgeCore; // ALT+159
+    ƒ.Debug.info("Main Program Template running!");
+    let viewport;
+    let root;
+    let laser;
+    document.addEventListener("interactiveViewportStarted", start);
+    function start(_event) {
+        viewport = _event.detail;
+        root = viewport.getBranch();
+        console.log(root);
+        laser = root.getChildrenByName("Laserformations")[0].getChildrenByName("Laserformation")[0].getChildrenByName("Laser01")[0];
+        ƒ.Loop.addEventListener("loopFrame" /* LOOP_FRAME */, update);
+        ƒ.Loop.start(); // start the game loop to continously draw the viewport, update the audiosystem and drive the physics i/a
+    }
+    function update(_event) {
+        // ƒ.Physics.world.simulate();  // if physics is included and used
+        viewport.draw();
+        ƒ.AudioManager.default.update();
+        laser.getComponent(ƒ.ComponentTransform).mtxLocal.rotateZ(0.5);
+    }
+})(Script || (Script = {}));
+//# sourceMappingURL=Script.js.map
--- Laser League/Script/Source/Main.ts
+++ Laser League/Script/Source/Main.ts
@@ -1,20 +1,29 @@
 namespace Script {
-  import ƒ = FudgeCore;
-  ƒ.Debug.info("Main Program Template running!")
+  // Don't forget to compile: Strg + Shift + B
+  import ƒ = FudgeCore; // ALT+159
+  ƒ.Debug.info("Main Program Template running!");
 
   let viewport: ƒ.Viewport;
+  let root: ƒ.Node;
+  let laser: ƒ.Node;
   document.addEventListener("interactiveViewportStarted", <EventListener>start);
 
   function start(_event: CustomEvent): void {
     viewport = _event.detail;
+    root = viewport.getBranch();
+    console.log(root);
+    laser = root.getChildrenByName("Laserformations")[0].getChildrenByName("Laserformation")[0].getChildrenByName("Laser01")[0];
 
     ƒ.Loop.addEventListener(ƒ.EVENT.LOOP_FRAME, update);
-    // ƒ.Loop.start();  // start the game loop to continously draw the viewport, update the audiosystem and drive the physics i/a
+    ƒ.Loop.start();  // start the game loop to continously draw the viewport, update the audiosystem and drive the physics i/a
   }
 
   function update(_event: Event): void {
     // ƒ.Physics.world.simulate();  // if physics is included and used
     viewport.draw();
     ƒ.AudioManager.default.update();
+    laser.getComponent(ƒ.ComponentTransform).mtxLocal.rotateZ(0.5);
   }
+
+
 }
