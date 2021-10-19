2ce2f1b2ea2087406636413433b03a271941401c
Markus Damm <44026453+MarkusDamm@users.noreply.github.com>
2021-10-19 15:45:17 +0200

    19.10.

--- Laser League/Script/Build/Script.js
+++ Laser League/Script/Build/Script.js
@@ -39,21 +39,51 @@ var Script;
     ƒ.Debug.info("Main Program Template running!");
     let viewport;
     let root;
+    let agent;
     let laser;
+    let fps = 60;
+    let deltaTime = ƒ.Loop.timeFrameReal / 1000;
+    let moveSpeed = 5;
+    let rotateSpeed = 60;
     document.addEventListener("interactiveViewportStarted", start);
     function start(_event) {
         viewport = _event.detail;
         root = viewport.getBranch();
         console.log(root);
         laser = root.getChildrenByName("Laserformations")[0].getChildrenByName("Laserformation")[0].getChildrenByName("Laser01")[0];
+        agent = root.getChildrenByName("Agents")[0].getChildrenByName("Agent01")[0];
         ƒ.Loop.addEventListener("loopFrame" /* LOOP_FRAME */, update);
-        ƒ.Loop.start(); // start the game loop to continously draw the viewport, update the audiosystem and drive the physics i/a
+        ƒ.Loop.start(ƒ.LOOP_MODE.TIME_REAL, fps); // start the game loop to continously draw the viewport, update the audiosystem and drive the physics i/a
+        // Adjust Camera Position
+        viewport.camera.mtxPivot.translateZ(-45);
     }
     function update(_event) {
         // ƒ.Physics.world.simulate();  // if physics is included and used
+        // Movements
+        if (ƒ.Keyboard.isPressedOne([ƒ.KEYBOARD_CODE.W, ƒ.KEYBOARD_CODE.ARROW_UP])) {
+            agent.mtxLocal.translateY(moveSpeed * deltaTime);
+            // agent.getComponent(ƒ.ComponentTransform).mtxLocal.translateY(0.3);
+            // begrenze Arena durch Kolissionen
+        }
+        if (ƒ.Keyboard.isPressedOne([ƒ.KEYBOARD_CODE.S, ƒ.KEYBOARD_CODE.ARROW_DOWN])) {
+            agent.mtxLocal.translateY(-moveSpeed * deltaTime);
+        }
+        if (ƒ.Keyboard.isPressedOne([ƒ.KEYBOARD_CODE.D])) {
+            agent.mtxLocal.translateX(moveSpeed * deltaTime);
+        }
+        if (ƒ.Keyboard.isPressedOne([ƒ.KEYBOARD_CODE.A])) {
+            agent.mtxLocal.translateX(-moveSpeed * deltaTime);
+        }
+        if (ƒ.Keyboard.isPressedOne([ƒ.KEYBOARD_CODE.ARROW_LEFT])) {
+            agent.mtxLocal.rotateZ(rotateSpeed * deltaTime);
+        }
+        if (ƒ.Keyboard.isPressedOne([ƒ.KEYBOARD_CODE.ARROW_RIGHT])) {
+            agent.mtxLocal.rotateZ(-rotateSpeed * deltaTime);
+        }
+        let laserRotationSpeed = 120;
+        laser.getComponent(ƒ.ComponentTransform).mtxLocal.rotateZ(laserRotationSpeed * deltaTime);
         viewport.draw();
         ƒ.AudioManager.default.update();
-        laser.getComponent(ƒ.ComponentTransform).mtxLocal.rotateZ(0.5);
     }
 })(Script || (Script = {}));
 //# sourceMappingURL=Script.js.map
--- Laser League/Script/Source/Main.ts
+++ Laser League/Script/Source/Main.ts
@@ -5,7 +5,13 @@ namespace Script {
 
   let viewport: ƒ.Viewport;
   let root: ƒ.Node;
+  let agent: ƒ.Node;
   let laser: ƒ.Node;
+
+  let fps: number = 60;
+  let deltaTime: number = ƒ.Loop.timeFrameReal / 1000;
+  let moveSpeed: number = 5;
+  let rotateSpeed: number = 60;
   document.addEventListener("interactiveViewportStarted", <EventListener>start);
 
   function start(_event: CustomEvent): void {
@@ -14,15 +20,52 @@ namespace Script {
     console.log(root);
     laser = root.getChildrenByName("Laserformations")[0].getChildrenByName("Laserformation")[0].getChildrenByName("Laser01")[0];
 
+    agent = root.getChildrenByName("Agents")[0].getChildrenByName("Agent01")[0];
     ƒ.Loop.addEventListener(ƒ.EVENT.LOOP_FRAME, update);
-    ƒ.Loop.start();  // start the game loop to continously draw the viewport, update the audiosystem and drive the physics i/a
+    ƒ.Loop.start(ƒ.LOOP_MODE.TIME_REAL, fps);  // start the game loop to continously draw the viewport, update the audiosystem and drive the physics i/a
+
+    // Adjust Camera Position
+    viewport.camera.mtxPivot.translateZ(-45);
+
   }
 
   function update(_event: Event): void {
     // ƒ.Physics.world.simulate();  // if physics is included and used
+
+    // Movements
+    if (ƒ.Keyboard.isPressedOne([ƒ.KEYBOARD_CODE.W, ƒ.KEYBOARD_CODE.ARROW_UP]))
+    {
+      agent.mtxLocal.translateY(moveSpeed * deltaTime);
+      // agent.getComponent(ƒ.ComponentTransform).mtxLocal.translateY(0.3);
+      // begrenze Arena durch Kolissionen
+    }
+    if (ƒ.Keyboard.isPressedOne([ƒ.KEYBOARD_CODE.S, ƒ.KEYBOARD_CODE.ARROW_DOWN]))
+    {
+      agent.mtxLocal.translateY(-moveSpeed * deltaTime);
+    }
+    if (ƒ.Keyboard.isPressedOne([ƒ.KEYBOARD_CODE.D]))
+    {
+      agent.mtxLocal.translateX(moveSpeed * deltaTime);
+    }
+    if (ƒ.Keyboard.isPressedOne([ƒ.KEYBOARD_CODE.A]))
+    {
+      agent.mtxLocal.translateX(-moveSpeed * deltaTime);
+    }
+    if (ƒ.Keyboard.isPressedOne([ƒ.KEYBOARD_CODE.ARROW_LEFT]))
+    {
+      agent.mtxLocal.rotateZ(rotateSpeed * deltaTime);
+    }
+    if (ƒ.Keyboard.isPressedOne([ƒ.KEYBOARD_CODE.ARROW_RIGHT]))
+    {
+      agent.mtxLocal.rotateZ(-rotateSpeed * deltaTime);
+    }
+    
+    let laserRotationSpeed: number = 120;
+    laser.getComponent(ƒ.ComponentTransform).mtxLocal.rotateZ(laserRotationSpeed * deltaTime);
+    
+    
     viewport.draw();
     ƒ.AudioManager.default.update();
-    laser.getComponent(ƒ.ComponentTransform).mtxLocal.rotateZ(0.5);
   }
 
 
