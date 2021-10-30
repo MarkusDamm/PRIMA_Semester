994607971339faa999e59f289325449f72507327
Markus Damm <markus.damm@hs-Furtwangen.de>
2021-10-30 17:01:40 +0200

    use ƒ.Random

--- Laser League/Script/Source/LaserScript.ts
+++ Laser League/Script/Source/LaserScript.ts
@@ -1,20 +1,20 @@
 namespace Script {
     import ƒ = FudgeCore;
     ƒ.Project.registerScriptNamespace(Script);  // Register the namespace to FUDGE for serialization
-  
+
     export class LaserScript extends ƒ.ComponentScript {
-        
+
         public message: string = "Laser Script added to ";
 
         private laserRotationSpeed: number = 120;
 
         constructor() {
             super();
-            
+
             // Don't start when running in editor
             if (ƒ.Project.mode == ƒ.MODE.EDITOR)
                 return;
-            
+
             // Listen to this component being added to or removed from a node
             this.addEventListener(ƒ.EVENT.COMPONENT_ADD, this.hndEvent);
             this.addEventListener(ƒ.EVENT.COMPONENT_REMOVE, this.hndEvent);
@@ -25,10 +25,10 @@ namespace Script {
             switch (_event.type) {
                 case ƒ.EVENT.COMPONENT_ADD:
                     console.log("add listener for hdlRotation");
-                    this.laserRotationSpeed = Math.random() * 80 + 40;
-                    if (Math.random() - 0.5 < 0) {
+                    let random: ƒ.Random = new ƒ.Random();
+                    this.laserRotationSpeed = random.getRangeFloored(40, 80);
+                    if (random.getBoolean())
                         this.laserRotationSpeed *= -1;
-                    }
                     ƒ.Loop.addEventListener(ƒ.EVENT.LOOP_FRAME, this.hndRotation);
                     break;
                 case ƒ.EVENT.COMPONENT_REMOVE:
@@ -40,7 +40,7 @@ namespace Script {
         }
 
         public hndRotation = (_event: Event) => {
-            let deltaTime: number = ƒ.Loop.timeFrameReal / 1000;      
+            let deltaTime: number = ƒ.Loop.timeFrameReal / 1000;
             this.node.getComponent(ƒ.ComponentTransform).mtxLocal.rotateZ(this.laserRotationSpeed * deltaTime)
         }
     }
