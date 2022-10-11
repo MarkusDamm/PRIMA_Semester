cc4d2b2505ce48a65e92329449d6c51dcc7784e7
Markus Damm <44026453+MarkusDamm@users.noreply.github.com>
2022-10-11 15:49:35 +0200

    add Luigi Link

--- Super_Luigi/Script/Source/Main.ts
+++ Super_Luigi/Script/Source/Main.ts
@@ -10,6 +10,11 @@ namespace Script {
 
     ƒ.Loop.addEventListener(ƒ.EVENT.LOOP_FRAME, update);
     // ƒ.Loop.start();  // start the game loop to continously draw the viewport, update the audiosystem and drive the physics i/a
+
+    let root : ƒ.Node = viewport.getBranch();
+    let luigi: ƒ.Node[] = root.getChildren();
+    console.log(luigi);
+    
   }
 
   function update(_event: Event): void {
