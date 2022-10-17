fd7d39d8d6618adc8e2afd6da5d20f5bb4a99c51
MarkusDamm <markus.damm@hs-furtwangen.de>
2022-10-17 15:19:43 +0200

    animate Luigi

--- Super_Luigi/Script/Build/Script.js
+++ Super_Luigi/Script/Build/Script.js
@@ -39,25 +39,48 @@ var Script;
 var Script;
 (function (Script) {
     var ƒ = FudgeCore;
+    var ƒAid = FudgeAid;
     ƒ.Debug.info("Main Program Template running!");
     let viewport;
     document.addEventListener("interactiveViewportStarted", start);
     let luigiPos;
-    function start(_event) {
+    async function start(_event) {
         viewport = _event.detail;
-        ƒ.Loop.addEventListener("loopFrame" /* LOOP_FRAME */, update);
         ƒ.Loop.start(); // start the game loop to continously draw the viewport, update the audiosystem and drive the physics i/a
         console.log(viewport);
+        // get Nodes
         let branch = viewport.getBranch();
-        console.log(branch);
         luigiPos = branch.getChildrenByName("LuigiPosition")[0];
-        console.log(luigiPos);
+        luigiPos.removeAllChildren();
+        // create Luigi
+        let luigiNode = new ƒAid.NodeSprite("Luigi");
+        luigiNode.addComponent(new ƒ.ComponentTransform());
+        luigiNode.mtxLocal.rotateY(180);
+        luigiNode.mtxLocal.translateY(-0.05);
+        luigiPos.appendChild(luigiNode);
+        // texture Luigi
+        let texture = new ƒ.TextureImage();
+        await texture.load("./Sprites/Luigi_Moves_Sheet2.png");
+        let coat = new ƒ.CoatTextured(ƒ.Color.CSS("white"), texture);
+        // animation
+        // Walk
+        let animWalk = new ƒAid.SpriteSheetAnimation("Walk", coat);
+        animWalk.generateByGrid(ƒ.Rectangle.GET(176, 38, 16, 32), 3, 32, ƒ.ORIGIN2D.TOPLEFT, ƒ.Vector2.X(52));
+        // Run
+        let animRun = new ƒAid.SpriteSheetAnimation("Run", coat);
+        animRun.generateByGrid(ƒ.Rectangle.GET(332, 38, 18, 32), 3, 32, ƒ.ORIGIN2D.TOPLEFT, ƒ.Vector2.X(52));
+        luigiNode.setAnimation(animWalk);
+        luigiNode.setFrameDirection(1);
+        luigiNode.framerate = 12;
+        ƒ.Loop.addEventListener("loopFrame" /* LOOP_FRAME */, update);
     }
     function update(_event) {
+        // move Luigi
+        let cmpTransL = luigiPos.getComponent(ƒ.ComponentTransform);
+        cmpTransL.mtxLocal.translateX(0.01);
         // ƒ.Physics.simulate();  // if physics is included and used
         viewport.draw();
         ƒ.AudioManager.default.update();
-        luigiPos.getComponent(ƒ.ComponentTransform).mtxLocal.translateX(0.01);
     }
 })(Script || (Script = {}));
 //# sourceMappingURL=Script.js.map
--- Super_Luigi/Script/Source/Main.ts
+++ Super_Luigi/Script/Source/Main.ts
@@ -1,5 +1,6 @@
 namespace Script {
   import ƒ = FudgeCore;
+  import ƒAid = FudgeAid;
 
   ƒ.Debug.info("Main Program Template running!");
 
@@ -7,22 +8,50 @@ namespace Script {
   document.addEventListener("interactiveViewportStarted", <EventListener>start);
   let luigiPos: ƒ.Node;
 
-  function start(_event: CustomEvent): void {
+  async function start(_event: CustomEvent): Promise<void> {
     viewport = _event.detail;
-    ƒ.Loop.addEventListener(ƒ.EVENT.LOOP_FRAME, update);
     ƒ.Loop.start();  // start the game loop to continously draw the viewport, update the audiosystem and drive the physics i/a
     console.log(viewport);
 
+    // get Nodes
     let branch: ƒ.Node = viewport.getBranch();
-    console.log(branch);
     luigiPos = branch.getChildrenByName("LuigiPosition")[0];
-    console.log(luigiPos);
+    luigiPos.removeAllChildren();
+
+    // create Luigi
+    let luigiNode: ƒAid.NodeSprite = new ƒAid.NodeSprite("Luigi");
+    luigiNode.addComponent(new ƒ.ComponentTransform());
+    luigiNode.mtxLocal.rotateY(180);
+    luigiNode.mtxLocal.translateY(-0.05);
+    luigiPos.appendChild(luigiNode);
+
+    // texture Luigi
+    let texture: ƒ.TextureImage = new ƒ.TextureImage();
+    await texture.load("./Sprites/Luigi_Moves_Sheet2.png");
+    let coat: ƒ.CoatTextured = new ƒ.CoatTextured(ƒ.Color.CSS("white"), texture);
+
+    // animation
+    // Walk
+    let animWalk: ƒAid.SpriteSheetAnimation = new ƒAid.SpriteSheetAnimation("Walk", coat);
+    animWalk.generateByGrid(ƒ.Rectangle.GET(176, 38, 16, 32), 3, 32, ƒ.ORIGIN2D.TOPLEFT, ƒ.Vector2.X(52));
+    // Run
+    let animRun: ƒAid.SpriteSheetAnimation = new ƒAid.SpriteSheetAnimation("Run", coat);
+    animRun.generateByGrid(ƒ.Rectangle.GET(332, 38, 18, 32), 3, 32, ƒ.ORIGIN2D.TOPLEFT, ƒ.Vector2.X(52));
+
+    luigiNode.setAnimation(animWalk);
+    luigiNode.setFrameDirection(1);
+    luigiNode.framerate = 12;
+
+    ƒ.Loop.addEventListener(ƒ.EVENT.LOOP_FRAME, update);
   }
 
   function update(_event: Event): void {
+    // move Luigi
+    let cmpTransL: ƒ.ComponentTransform = luigiPos.getComponent(ƒ.ComponentTransform);
+    cmpTransL.mtxLocal.translateX(0.01);
+
     // ƒ.Physics.simulate();  // if physics is included and used
     viewport.draw();
     ƒ.AudioManager.default.update();
-    luigiPos.getComponent(ƒ.ComponentTransform).mtxLocal.translateX(0.01);
   }
 }
