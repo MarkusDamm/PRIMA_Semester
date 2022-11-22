09476c83362af47385003c3e32ee4aacf963a38d
Markus Damm <44026453+MarkusDamm@users.noreply.github.com>
2022-11-22 17:14:21 +0100

    end of lecture

--- BarrelRoll/Script/Source/Main.ts
+++ BarrelRoll/Script/Source/Main.ts
@@ -1,20 +1,30 @@
 namespace Script {
   import ƒ = FudgeCore;
-  ƒ.Debug.info("Main Program Template running!");
-
+  // Steuerung einbauen
+  
   let viewport: ƒ.Viewport;
   document.addEventListener("interactiveViewportStarted", <EventListener>start);
+
+  let rbShip: ƒ.ComponentRigidbody;
 
   function start(_event: CustomEvent): void {
     viewport = _event.detail;
 
     ƒ.Loop.addEventListener(ƒ.EVENT.LOOP_FRAME, update);
-    // ƒ.Loop.start();  // start the game loop to continously draw the viewport, update the audiosystem and drive the physics i/a
+    ƒ.Loop.start();  // start the game loop to continously draw the viewport, update the audiosystem and drive the physics i/a
+
+    let graph: ƒ.Node = viewport.getBranch();
+    let ship: ƒ.Node = graph.getChildrenByName("Fox")[0].getChild(0);
+    console.log(ship);
+    rbShip = ship.getComponent(ƒ.ComponentRigidbody);
+
   }
 
   function update(_event: Event): void {
-    // ƒ.Physics.simulate();  // if physics is included and used
+    ƒ.Physics.simulate();  // if physics is included and used
     viewport.draw();
     ƒ.AudioManager.default.update();
+
+    rbShip.applyForce(ƒ.Vector3.Z(rbShip.mass * 5));
   }
 }
